95052ebfe855b0479ff7b17d57ae75a6c61eade6
sparsh sharma <sparshsharma0303@gmail.com>
2026-02-19 20:55:34 +0530

    predict pipline and templates

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+import dill
+
 
 from sklearn.model_selection import GridSearchCV
 
@@ -54,4 +56,13 @@ def evaluate_models(X_train,y_train,X_test,y_test, models,param):
         raise CustomException(e,sys)
 
         
-
+def load_object(file_path):
+    try:
+        with open(file_path,"rb" ) as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e :
+        raise CustomException(e,sys)
+    
+    
+        
